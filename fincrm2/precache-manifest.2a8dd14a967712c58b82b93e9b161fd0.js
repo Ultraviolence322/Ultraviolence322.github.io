1b484baca0d113b7f54bf96c713cb40ad2ffbc94
Ultraviolence322 <segeichenkov18@gmail.com>
2022-01-31 09:56:54 +0300

    fincrm 2

---
+++ fincrm2/precache-manifest.2a8dd14a967712c58b82b93e9b161fd0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056ee7dadcf40229c174",
+    "url": "/css/app.e6b4037b.css"
+  },
+  {
+    "revision": "26321990e07dd3b720fa",
+    "url": "/css/chunk-1424f6bc.238cf023.css"
+  },
+  {
+    "revision": "823c899c39258753e437",
+    "url": "/css/chunk-1736fcc1.e7b3562c.css"
+  },
+  {
+    "revision": "697572c54321d35c70e6",
+    "url": "/css/chunk-364c4450.de7007c1.css"
+  },
+  {
+    "revision": "0f51115d3e0ab0b2f236",
+    "url": "/css/chunk-73b9e704.70152a49.css"
+  },
+  {
+    "revision": "801cae58e266f0440ed1",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "43b2034e0f0094527096def257a1dca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "056ee7dadcf40229c174",
+    "url": "/js/app.bc47cd1c.js"
+  },
+  {
+    "revision": "6dbff3cc36ffecf4cfee",
+    "url": "/js/chunk-0bd44d87.6843c659.js"
+  },
+  {
+    "revision": "551d577f70af708beb70",
+    "url": "/js/chunk-1031e649.2082db09.js"
+  },
+  {
+    "revision": "26321990e07dd3b720fa",
+    "url": "/js/chunk-1424f6bc.0fd15500.js"
+  },
+  {
+    "revision": "823c899c39258753e437",
+    "url": "/js/chunk-1736fcc1.2da770d4.js"
+  },
+  {
+    "revision": "9b65c99ba80e70ee8431",
+    "url": "/js/chunk-1f8a5e0e.24df11d9.js"
+  },
+  {
+    "revision": "4a5332b6c9c14679313f",
+    "url": "/js/chunk-29405feb.5c45d4fe.js"
+  },
+  {
+    "revision": "1d100da16ae555a4daeb",
+    "url": "/js/chunk-2d0d61f1.fcf09ae2.js"
+  },
+  {
+    "revision": "697572c54321d35c70e6",
+    "url": "/js/chunk-364c4450.3c37cf08.js"
+  },
+  {
+    "revision": "0f51115d3e0ab0b2f236",
+    "url": "/js/chunk-73b9e704.aa2ca180.js"
+  },
+  {
+    "revision": "801cae58e266f0440ed1",
+    "url": "/js/chunk-vendors.efa6334a.js"
+  },
+  {
+    "revision": "e2c353512466db059a2fb58392810faa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
